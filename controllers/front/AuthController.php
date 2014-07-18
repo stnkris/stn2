0e5009d19d8adee60b50013d5bbb6a7fb05e4c3d
Kristopher Ives <kristopher.ives@stoysnet.com>
2014-07-17 17:27:47 -0700

    Removed passwords from email that are sent upon registration.

--- controllers/front/AuthController.php
+++ controllers/front/AuthController.php
@@ -720,7 +720,7 @@ class AuthControllerCore extends FrontController
 				'{firstname}' => $customer->firstname,
 				'{lastname}' => $customer->lastname,
 				'{email}' => $customer->email,
-				'{passwd}' => Tools::getValue('passwd')),
+			),
 			$customer->email,
 			$customer->firstname.' '.$customer->lastname
 		);
